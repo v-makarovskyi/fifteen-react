083921ebc8fc33f32f15d6105fe8d38a1acde3e1
Vladimir <bigcorvet@gmail.com>
2023-06-17 17:56:19 +0300

    added shuffle, matrix

--- static/js/index.js
+++ static/js/index.js
@@ -1,68 +1,97 @@
-const containerNode = document.getElementById("fifteen");
-const itemNodes = Array.from(containerNode.querySelectorAll(".fifteen__item"));
-const countItems = 16;
-
-if (itemNodes.length !== 16) {
-  throw new Error(`Должно быть ровно ${countItems} элементов в HTML`);
-}
-
-/* ---------------------- position ---------------------- */
-
-itemNodes[countItems - 1].style.display = 'none'
-
-const matrix = getMatrix(itemNodes.map((item) => item.dataset.matrixId));
-setPositionItems(matrix);
+const containerNodes = document.getElementById('fifteen')
+const itemsNodes = Array.from(containerNodes.querySelectorAll('.item'))
+const countItemsNodes = 16
 
 
-/* ---------------------- shuffle ---------------------- */
 
-/* ---------------------- helpers ---------------------- */
+/* Position */
+let matrix = getMatrix(itemsNodes.map((item) => Number(item.dataset.matrixId)))
+setPositionItems(matrix)
+itemsNodes[countItemsNodes - 1].style.display = 'none' /* скрыть 16-й элемент */
 
-function getMatrix(arr) {
-  const matrix = [[], [], [], []];
 
-  let y = 0;
-  let x = 0;
-
-  for (let i = 0; i < arr.length; i++) {
-    if (x >= 4) {
-      y++;
-      x = 0;
+/* Shuffle */
+document.getElementById('shuffle').addEventListener('click', () => {
+    const shuffledArr = shuffledArray(matrix.flat())
+    matrix = getMatrix(shuffledArr)
+    setPositionItems(matrix)
+})
+/* Handle change position by click */
+const blankNumber = 16
+containerNodes.addEventListener('click', (event) => {
+    const buttonNode = event.target.closest('button')
+    if(!buttonNode) {
+        return
     }
-    matrix[y][x] = arr[i];
-    x++;
-  }
-  return matrix;
+    const buttonNumber = Number(buttonNode.dataset.matrixId)
+    const buttonCoords = findCoordsByNymber(buttonNumber, matrix)
+    const blankCoords = findCoordsByNymber(blankNumber, matrix)
+    
+    console.log(buttonCoords)
+    console.log(blankCoords)
+
+    const isValid = isValidForSwap(buttonCoords, blankCoords)
+    console.log(isValid)
+    /* isValid(valid) {
+
+    } */
+})
+
+/* Handle change position by kewdown */
+
+/** HELPERS**/
+function getMatrix(arr) {
+    let matrix = [[], [], [], []]
+    let y = 0
+    let x = 0
+    for(let i = 0; i < arr.length; i++) {
+        if(x >= 4) {
+            y++
+            x = 0
+        }
+        matrix[y][x] = arr[i]
+        x++
+    }
+    return matrix
 }
 
 function setPositionItems(matrix) {
-  for (let y = 0; y < matrix.length; y++) {
-    for (let x = 0; x < matrix[y].length; x++) {
-      const value = matrix[y][x];
-      const node = itemNodes[value - 1];
-      setNodeStyle(node, x, y);
+    for(let y = 0; y < matrix.length; y++) {
+        for(let x = 0; x < matrix[y].length; x++) {
+            const value = matrix[y][x]
+            const node = itemsNodes[value - 1]
+            setNodesStyles(node, x, y)
+        }
     }
-  }
 }
 
-function setNodeStyle(node, x, y) {
-  const shiftPs = 100;
-  node.style.transform = `translate3D(${x * shiftPs}%, ${y * shiftPs}%, 0)`;
+function setNodesStyles(node, x, y) {
+    const shiftPs = 100
+    node.style.transform = `translate3D(${x*shiftPs}%, ${y*shiftPs}%, 0)`
 }
 
-//1. matrix[0][0] = arr[0]
-//2. matrix[0][1] = arr[1]
-//3 matrix[0][2] = arr[2]
-//4 matrix[0][3] = arr[3]
-//5 matrix[1][0] = arr[4]
-//6 matrix[1][1] = arr[5]
-//7 matrix[1][2] = arr[6]
-//8 matrix[1][3] = arr[7]
-//9 matrix[2][0] = arr[8]
-//10 matrix[2][1] = arr[9]
-//11 matrix[2][2] = arr[10]
-//12 matrix[2][3] = arr[11]
-//12 matrix[3][0] = arr[12]
-//12 matrix[3][1] = arr[13]
-//12 matrix[3][2] = arr[14]
-//12 matrix[3][3] = arr[15]
+function shuffledArray(arr) {
+    return arr
+        .map(value => ({value, sort: Math.random()}))
+        .sort((a,b) => a.sort - b.sort)
+        .map(({value}) => value)
+}
+
+function findCoordsByNymber(number, matrix) {
+    for(let y = 0; y < matrix.length; y++) {
+        for(let x = 0; x < matrix[y].length; x++) {
+            if(matrix[y][x] === number) {
+                return {x, y}
+            }
+
+        }
+    }
+    return null
+}
+
+function isValidForSwap(coords1, coords2) {
+    const diffX = Math.abs(coords1.x - coords2.x)
+    const diffY = Math.abs(coords1.y - coords2.y)
+
+    return (diffX === 1 || diffY ===1) && (coords1.x === coords2.x || coords1.y === coords2.y)
+}
